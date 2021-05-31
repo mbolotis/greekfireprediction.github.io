1887b243eef555693cb28b7078a00b48fb3ba0ce
Michael Bolotis <michael-94@windowslive.com>
2021-06-01 00:45:06 +0300

    Update map

--- init.bat
+++ init.bat
@@ -2,3 +2,4 @@ python C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfirep
 copy /y C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\sa_map.html C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfireprediction.github.io\index.html
 cd C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfireprediction.github.io
 call git status && git add . && git commit -m "Update map" && git push
+PAUSE
