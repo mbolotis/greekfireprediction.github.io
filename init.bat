da559e654e4a06becf178666b0353af6a9e7ebb6
Michael Bolotis <michael-94@windowslive.com>
2022-02-05 14:03:54 +0200

    Update map

--- init.bat
+++ init.bat
@@ -1,7 +1,4 @@
 python C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfireprediction.github.io\run_map.py
 copy /y C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\sa_map.html C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfireprediction.github.io\index.html
 cd C:\Users\hppc\Desktop\GitHub\standalone_wildfireprediction\map\greekfireprediction.github.io
-call git status && git add . && git commit -m "Update map"
-call git push
-Username: michael-94@windowslive.com
-Password: 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
+call git status && git add . && git commit -m "Update map" && git push
