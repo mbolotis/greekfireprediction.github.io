611037fb3918c01415df073912dfb810621807d4
Michael Bolotis <michael-94@windowslive.com>
2023-07-13 00:07:02 +0300

    Update zoom exec_map.py

--- exec_map.py
+++ exec_map.py
@@ -277,7 +277,7 @@ if __name__ == "__main__":
       country = i
       break
   m = folium.Map(location=[37.97025, 23.72247],
-           zoom_start=6.5,
+           zoom_start=6,
            min_zoom=6,
            max_zoom=7,
            scrollWheelZoom=False,
